73007a94c77e7f5687100c67f2e4bd9e7fa9fd94
madhubabu484 <madhubabu@srnritsolutions.com>
2025-06-27 11:29:46 +0530

    Modified changes

--- src/main/java/com/CineReserve/BookingDTO/BookingDTO.java
+++ src/main/java/com/CineReserve/BookingDTO/BookingDTO.java
@@ -10,28 +10,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BookingDTO {
-	
-	
-	
+
 	private Long userid;
-	
+
 	private String bookingid;
-	
+
 	private Long movie;
-	
+
 	private Long theatre;
-	
+
 	private String seatNumber;
-	
+
 	private String status;
-	
+
 	private String reason;
-	
+
 	private LocalDate creat;
-	
-	
-	
-	
-	
 
 }
